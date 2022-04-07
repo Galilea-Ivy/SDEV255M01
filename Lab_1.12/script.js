705afe7bae0bc924e2189237dc79d82b28fb6865
Galilea-Ivy <ggarcia87@ivytech.edu>
2022-04-07 17:51:17 -0400

    M01 lab 1.12 changes

--- Lab_1.12/script.js
+++ Lab_1.12/script.js
@@ -1,22 +1,24 @@
-// Your solution goes here 
-function playGuessingGame(numToGuess, totalGuess =10) {
-    let userGuess = prompt("Enter a number between 1 and 100.");
+// Your solution goes here
+function playGuessingGame(numToGuess, totalGuess = 10) {
+  let userGuess = prompt("Enter a number between 1 and 100.");
+  let numOfGuesses = 0;
 
-    for(i = 0; i < totalGuess.length; i++){
-        if(userGuess < numToGuess){
-            userGuess = prompt(userGuess + " is too small. Guess a larger number.");
-        }
-        else if(userGuess > numToGuess){
-            userGuess = prompt(userGuess + " is too large. Guess a smaller number.");
-        }
-        else if(isNaN(userGuess)){
-            userGuess = prompt("Please enter a number."); 
-        }
-        else if(userGuess == numToGuess){
-            return i++;
-        } 
-        else {
-            return 0;
-        }
+  while (numOfGuesses != totalGuess) {
+    if (userGuess == null) {
+      return 0;
+    } else if (isNaN(userGuess) || userGuess == "") {
+      userGuess = prompt("Please enter a number.");
+    } else if (userGuess < numToGuess) {
+      userGuess = prompt(userGuess + " is too small. Guess a larger number.");
+      numOfGuesses++;
+    } else if (userGuess > numToGuess) {
+      userGuess = prompt(userGuess + " is too large. Guess a smaller number.");
+      numOfGuesses++;
+    } else if (userGuess == numToGuess) {
+      return numOfGuesses + 1;
+    } else {
+      break;
     }
+  }
+  return 0;
 }
